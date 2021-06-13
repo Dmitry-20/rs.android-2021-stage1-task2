2fd842ac75d45aeb866ee024f1d4938a7be92c85
“Dmitry <yakub.dm@mail.ru>
2021-06-13 17:48:19 +0300

    add task result

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,44 @@
 package subtask1
-
+import java.time.DateTimeException
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
 class DateFormatter {
 
-    // TODO: Complete the following function
+
     fun toTextDay(day: String, month: String, year: String): String {
+        val dayNumber = day.toInt()
+        val monthNumber = month.toInt()
+        val yearNumber = year.toInt()
+        var resultString: String
+        try {
+            val date = LocalDate.of(yearNumber, monthNumber, dayNumber)
+            resultString = day
+            resultString += when (date.month){
+                Month.JANUARY -> " января, "
+                Month.FEBRUARY -> " февраля, "
+                Month.MARCH -> " марта, "
+                Month.APRIL -> " апреля, "
+                Month.MAY -> " мая, "
+                Month.JUNE -> " июня, "
+                Month.JULY -> " июля, "
+                Month.AUGUST -> " августа, "
+                Month.SEPTEMBER -> " сентября, "
+                Month.OCTOBER -> " октября, "
+                Month.NOVEMBER -> " ноября, "
+                Month.DECEMBER -> " декабря, "
+            }
+            resultString += when (date.dayOfWeek){
+                DayOfWeek.MONDAY -> "понедельник"
+                DayOfWeek.TUESDAY -> "вторник"
+                DayOfWeek.WEDNESDAY -> "среда"
+                DayOfWeek.THURSDAY -> "четверг"
+                DayOfWeek.FRIDAY -> "пятница"
+                DayOfWeek.SATURDAY -> "суббота"
+                DayOfWeek.SUNDAY -> "воскресенье"
+            }
+        } catch (e: DateTimeException){return "Такого дня не существует"}
+        return resultString
         throw NotImplementedError("Not implemented")
     }
 }
--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,17 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
+
     fun abbreviationFromA(a: String, b: String): String {
+        var m=0
+        for (i in a){
+            if ( b[m]==i || b[m].toLowerCase()==i){
+                m++
+                if (m==b.length) return "YES"
+            }
+        }
+        return  "NO"
         throw NotImplementedError("Not implemented")
     }
 }
+//|| b[m].toLowerCase()==i   b[m]==i
